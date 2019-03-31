b7fba6fed41b900a7886498c291afef047e8713c
davisr137 <davisr137@gmail.com>
2019-03-31 11:02:43 -0700

    merge sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,3 +1,8 @@
+import math
+
+# The sentinel is a value larger than any other possible 
+# value in an array. Once we have reached the sentinel 
+# in a sorted array, we have reached the end. 
 SENTINEL = 10**10
 
 def merge(A, p, q, r):
@@ -26,4 +31,27 @@ def merge(A, p, q, r):
         else:
             A[k] = R[j]
             j += 1
+    return A
+
+def merge_sort(A, p, r):
+    """
+    Perform merge sort on sub-array of array A between 
+    elements indexed p and r. 
+
+    Args:
+        A (list of int): Our array.
+        p (int)
+        r (int)
+
+    Returns: 
+        A (list of int): Sorted array.
+    """
+    # Stop if we have a length-1 array -> already sorted
+    if p < r-1:
+        # Partition into two subarrays and merge each separately. 
+        q = int(math.floor((p+r) / 2.0)) 
+        A = merge_sort(A, p, q)
+        A = merge_sort(A, q, r)
+        # Merge the sorted sub-arrays.
+        A = merge(A, p, q, r)
     return A
