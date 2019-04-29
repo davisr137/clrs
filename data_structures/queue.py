77e6c8e636833e34d49d7a7a0caf08a0c47d7336
davisr137 <davisr137@gmail.com>
2019-04-28 18:49:46 -0700

    double-ended queue (deque)

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -45,3 +45,74 @@ class Queue(object):
         x = self.Q[self.head]
         self.head = (self.head + 1) % self.n
         return x
+
+
+class Deque(Queue):
+    """
+    Array implementation of a deque (double-ended queue).
+    Allow insertion and deletion at both ends in O(1) time.
+    
+    CLRS Exercise 10.1-5
+    """
+    def insert_right(self, x):
+        """
+        Insert element x at right end of deque.
+        
+        Args:
+            x (int)
+        """
+        if (self.tail + 1) % self.n == self.head:
+            raise AssertionError("Overflow")
+        self.Q[self.tail] = x
+        self.tail = (self.tail + 1) % self.n
+
+    def insert_left(self, x):
+        """
+        Insert element x at left end of deque.
+        
+        Args:
+            x (int)
+        """
+        if self.head == 0:
+            head_new = self.n-1
+        else:
+            head_new = self.head-1
+        if head_new == self.tail:
+            raise AssertionError("Overflow")
+        self.Q[head_new] = x
+        self.head = head_new
+
+    def delete_right(self):
+        """
+        Delete element from right end of deque.
+        
+        Returns:
+            int
+        """
+        if self.is_empty:
+            raise AssertionError("Underflow!")
+        if self.tail == 0:
+            tail_new = self.n-1
+        else:
+            tail_new = self.tail-1
+        x = self.Q[tail_new]
+        self.Q[tail_new] = None
+        self.tail = tail_new
+        return x
+
+    def delete_left(self):
+        """
+        Delete element from left end of deque.
+        
+        Returns:
+            int
+        """
+        if self.is_empty:
+            raise AssertionError("Underflow!")
+        x = self.Q[self.head]
+        self.Q[self.head] = None
+        self.head = (self.head + 1) % self.n
+        return x
+
+
+
