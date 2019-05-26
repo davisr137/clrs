4921f441006886971c22c726a0221738641e850c
davisr137 <davisr137@gmail.com>
2019-05-25 19:58:31 -0700

    sorted array to BST, tree walks

--- data_structures/tree.py
+++ data_structures/tree.py
@@ -1,3 +1,8 @@
+import math
+from typing import List
+
+## CLRS Chapter 12: Binary Search Trees
+
 class BinaryNode:
     """
     Class for node in binary search tree.
@@ -8,26 +13,74 @@ class BinaryNode:
         """
         self.val = val
         self.p = None # Parent
-        self.left_child = None # Left child
-        self.right_child = None # Right child
-    def add_parent(self, parent: BinaryNode):
+        self.left = None # Left child
+        self.right = None # Right child
+    def add_parent(self, parent):
         """
         Add parent.
         """
         self.parent = parent
-    def add_left_child(self, left_child: BinaryNode):
+    def add_left(self, left):
         """
         Add left child. 
         """
-        if left_child.val > self.val:
+        if left.val > self.val:
             s = "Left child value must be less than or equal to node value."
             raise AssertionError(s)
-        self.left_child = left_child
-    def add_right_child(self, right_child: BinaryNode):
+        self.left = left
+    def add_right(self, right):
         """
         Add right child.
         """
-        if right_child.val < self.val:
+        if right.val < self.val:
             s = "Right child value must be greater than or equal to node value."
             raise AssertionError(s)
+        self.right = right
 
+def array_to_bst(arr: List[int]) -> BinaryNode:
+    """
+    Convert sorted array to binary search tree.
+    """
+    if not arr:
+        return None
+    # Make root from midpoint of array
+    L = len(arr)
+    mid = math.floor(L/2)
+    root = BinaryNode(arr[mid])
+    # Recursively call function on left and right arrays
+    left_half = arr[:mid]
+    if left_half:
+        left = array_to_bst(left_half)
+        root.add_left(left)
+    right_half = arr[mid+1:]
+    if right_half:
+        right = array_to_bst(right_half)
+        root.add_right(right)
+    return root
+
+def inorder_tree_walk(node: BinaryNode) -> None:
+    """
+    Perform an in-order tree walk / traversal.
+    """
+    if node:
+        inorder_tree_walk(node.left)
+        print(node.val)
+        inorder_tree_walk(node.right)
+
+def preorder_tree_walk(node: BinaryNode) -> None:
+    """
+    Perform a pre-order tree walk / traversal.
+    """
+    if node:
+        print(node.val)
+        inorder_tree_walk(node.left)
+        inorder_tree_walk(node.right)
+
+def postorder_tree_walk(node: BinaryNode) -> None:
+    """
+    Perform a post-order tree walk / traversal.
+    """
+    if node:
+        inorder_tree_walk(node.left)
+        inorder_tree_walk(node.right)
+        print(node.val)
